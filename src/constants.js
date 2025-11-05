7da48c5a4ac3a61e18a70b5858608bb2af2d934d
Sagar Singh <sagar2021csai042@abesit.edu.in>
2025-11-05 12:33:05 +0530

    Resolve merge conflict in src/constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,4 @@
 // Skills Section Logo's
-<<<<<<< HEAD
-import htmlLogo from './assets/tech_logo/html.png';
-import cssLogo from './assets/tech_logo/css.png';
-import sassLogo from './assets/tech_logo/sass.png';
-import javascriptLogo from './assets/tech_logo/javascript.png';
-import reactjsLogo from './assets/tech_logo/reactjs.png';
-import angularLogo from './assets/tech_logo/angular.png';
-import reduxLogo from './assets/tech_logo/redux.png';
-import nextjsLogo from './assets/tech_logo/nextjs.png';
-import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
-import gsapLogo from './assets/tech_logo/gsap.png';
-import materialuiLogo from './assets/tech_logo/materialui.png';
-import bootstrapLogo from './assets/tech_logo/bootstrap.png';
-import springbootLogo from './assets/tech_logo/springboot.png';
-=======
 
 import htmlLogo from './assets/tech_logo/html.png';
 import cssLogo from './assets/tech_logo/css.png';
@@ -23,52 +8,26 @@ import reduxLogo from './assets/tech_logo/redux.png';
 import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
 
 import bootstrapLogo from './assets/tech_logo/bootstrap.png';
->>>>>>> e9a2548f1a28e44347c3f6522c29cb2972a9ba91
 import nodejsLogo from './assets/tech_logo/nodejs.png';
 import expressjsLogo from './assets/tech_logo/express.png';
 import mysqlLogo from './assets/tech_logo/mysql.png';
 import mongodbLogo from './assets/tech_logo/mongodb.png';
-<<<<<<< HEAD
-import firebaseLogo from './assets/tech_logo/firebase.png';
-import cLogo from './assets/tech_logo/c.png';
-import cppLogo from './assets/tech_logo/cpp.png';
-import javaLogo from './assets/tech_logo/java.png';
 import pythonLogo from './assets/tech_logo/python.png';
-import typescriptLogo from './assets/tech_logo/typescript.png';
-=======
-import pythonLogo from './assets/tech_logo/python.png';
->>>>>>> e9a2548f1a28e44347c3f6522c29cb2972a9ba91
 import gitLogo from './assets/tech_logo/git.png';
 import githubLogo from './assets/tech_logo/github.png';
 import vscodeLogo from './assets/tech_logo/vscode.png';
 import postmanLogo from './assets/tech_logo/postman.png';
-<<<<<<< HEAD
-import mcLogo from './assets/tech_logo/mc.png';
-import figmaLogo from './assets/tech_logo/figma.png';
-import netlifyLogo from './assets/tech_logo/netlify.png';
-import vercelLogo from './assets/tech_logo/vercel.png';
-import postgreLogo from './assets/tech_logo/postgre.png';
-import csharpLogo from './assets/tech_logo/csharp.png';
-
-// Experience Section Logo's
-import webverseLogo from './assets/company_logo/webverse_logo.png';
-import agcLogo from './assets/company_logo/agc_logo.png';
-=======
 import netlifyLogo from './assets/tech_logo/netlify.png';
 import vercelLogo from './assets/tech_logo/vercel.png';
 
 // Experience Section Logo's
->>>>>>> e9a2548f1a28e44347c3f6522c29cb2972a9ba91
 import goFour from './assets/company_logo/go4.png'
 import shivAnsh from './assets/company_logo/shiwansh_solutions.jpeg'
 
 
 // Education Section Logo's
 import glaLogo from './assets/education_logo/gla_logo.png';
-<<<<<<< HEAD
 import bsaLogo from './assets/education_logo/bsa_logo.png';
-=======
->>>>>>> e9a2548f1a28e44347c3f6522c29cb2972a9ba91
 import vpsLogo from './assets/education_logo/vpsLogo.png';
 
 // Project Section Logo's
